ff2be81c787bf0f820241c69fd7a3ae941182f8d
Lorrainelyp <1203571649@qq.com>
2019-06-17 22:44:40 +0800

    登录，退出redux

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+function mapStateToProps(state) {
+  return {}
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    outLogin:function (history) {
+      dispatch({type:"OUT_LOGIN"});
+      history.push('/')
+    }
+  };
+}
 class Home extends Component {
   render() {
     return (
       <div>
         <h3>主页</h3>
+        <div>
+          <button onClick={this.outLogin.bind(this)}>退出登录</button>
+        </div>
       </div>
     );
+  }
+
+  outLogin(){
+    this.props.outLogin(this.props.history);
   }
 
   componentDidMount() {
     console.log("Home渲染完毕")
   }
 }
-export default Home
+export default connect(mapStateToProps,mapDispatchToProps)(Home);
--- src/page/login/Login.js
+++ src/page/login/Login.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-class Login extends Component {
+import { connect } from "react-redux";
 
+function mapStateToProps(state) {
+  return {}
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    gologin:function (username,password,history) {
+      setTimeout(()=>{
+        console.log("username:"+username);
+        console.log("password:"+password);
+        dispatch({type:"GO_LOGIN"});
+        history.push('/Home');
+      },0)
+    }
+  }
+}
+
+class Login extends Component {
   render() {
     return (
       <div>
         <h3>登录页面</h3>
         <div>
-          用户名<input type="text" />
+          用户名<input type="text" ref="username"/>
         </div>
         <div>
-          密码<input type="text" />
+          密码<input type="text" ref="password" />
         </div>
         <div>
-          <button onClick={this.goLogin}>登录</button>
+          <button onClick={this.goLogin.bind(this)}>登录</button>
         </div>
       </div>
     );
   }
 
   goLogin(){
-    alert("开始登录")
+    this.props.gologin(this.refs.username.value,this.refs.password.value,this.props.history);
   }
 
   componentDidMount() {
@@ -28,4 +46,4 @@ class Login extends Component {
 
 }
 
-export default Login
+export default connect(mapStateToProps,mapDispatchToProps)(Login);
